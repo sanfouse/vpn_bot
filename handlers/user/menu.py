9b07f2be631edae0897daf5c3b99ca92c169d817
root <gfgfuyyy@gmail.com>
2022-12-18 18:14:35 +0000

    servers

--- handlers/user/menu.py
+++ handlers/user/menu.py
@@ -3,17 +3,22 @@ from configparser import ConfigParser
 from aiogram import types
 from loader import bot, dp
 from utils.db.manager_database import Peers, User, db
+import requests
+import os
 
 
 async def get_config(count, user_id):
     config = ConfigParser()
-    path = f'/root/wireguard/config/peer{count + 1}/peer{count + 1}.conf'
-    config.read(path)
+    path = f"http://194.87.219.96:8080/peer{count + 1}/peer{count + 1}.conf"
+    responce = requests.get(path)
+    open(f'/home/vpn_bot/handlers/user/files/{user_id}.conf', 'wb').write(responce.content)
+    config.read(f'/home/vpn_bot/handlers/user/files/{user_id}.conf')
     data_1 = dict(config.items('Interface'))
     data_2 = dict(config.items('Peer'))
     ip = data_1['address']
     publickey = data_2['publickey']
     await Peers.create(ip=ip, publickey=publickey, user_id=user_id, path=path)
+    os.remove(f'/home/vpn_bot/handlers/user/files/{user_id}.conf')
     return path
 
     
@@ -46,9 +51,7 @@ async def start(message: types.Message):
     if data is None:
         count = await db.func.count(Peers.publickey).gino.scalar()
         data = await get_config(count, message.from_user.id)
-
-    with open(data, 'rb') as file:
-        await bot.send_document(
-                document=file,
-                chat_id=message.chat.id
-            )
+    await bot.send_document(
+            document=types.InputFile.from_url(data),
+            chat_id=message.chat.id
+        )
