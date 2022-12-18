eda26fb26c1162d2e7cb565b6b7b9b7868c40d15
sanfit <94311113+sanfouse@users.noreply.github.com>
2022-12-18 16:41:24 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ docker -v
 echo "Check docker-compose installation status...."
 
 echo "Run installation..."
-git clone https://github.com/sanfouse/vpn_bot.git
 docker-compose up -d
 
 echo "Installation finish with status success..."
